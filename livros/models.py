a37ec3e57aff02b4a040c73d371e0a7707f06bef
CarolinaPinheiroSantos <pinheirocarol0506@gmail.com>
2025-04-04 10:27:58 -0300

    04/04/25

--- livros/models.py
+++ livros/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Livros(models.Model):
+class Livro(models.Model):
     titulo = models.CharField(max_length=50)
     autor = models.CharField(max_length=30)
     paginas = models.IntegerField(default=0)
--- livros/urls.py
+++ livros/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listar_livros, name='livros'),
+    path('livros/', views.listar_livros, name='livros'),
     path('api/livros/', views.listar_adicionar_livros),
 ]
